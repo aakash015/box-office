dd1641080ba7e831aa029048b2f3bf83032ffc38
Aakash <tiwaryaakash00@gmail.com>
2021-08-02 13:32:27 +0530

    dispayed the results from api and misc compo generated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,44 +1,66 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config';
 
 const Home = () => {
 
-   console.log("I am rerendered");
+ 
 
   const [input,setInput] = useState('');
+  const [results,setResults] = useState(null);
 
   function onInputChange(event){
      
-    //  console.log(input)
+    
      setInput(event.target.value);
 
   }
 
   function onSearch()
   {
-    // https://api.tvmaze.com/search/shows?q=men
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-    .then(
-      (response)=>{
-         return response.json();
-      }
-    )
-    .then((response)=>{
-      // console.log('@@@')
-      console.log(response)
+    apiGet(`/search/shows?q=${input}`).then((response)=>{
+     
+      setResults(response);
     })
 
+   
+   
   }
   
   function onKeyDown(event){
      if(event.keyCode===13)
       onSearch();
   }
+
+  function renderResults(){
+     
+       if(results && results.length===0)
+         return <div>No Results</div>
+       else
+       if(results && results.length>0)
+        {
+          
+          return ( 
+             <div>
+               {
+               results.map((elements)=>{
+                  return <div key = {elements.show.id}>{elements.show.name}</div>     
+               })
+              }
+             </div>
+          )
+        }
+
+        return null;
+
+  }
   return (
     <div>
       <MainPageLayout>
       <input type = "text" onKeyDown = {onKeyDown} onChange = {(event)=>onInputChange(event)} value = {input} />
       <button type="button" onClick = {onSearch}>Search</button>
+
+      {renderResults()}
       </MainPageLayout> 
     </div>
   )
