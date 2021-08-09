f9489a1c030439880d31b23bf769723526a4e195
Aakash <tiwaryaakash00@gmail.com>
2021-08-09 23:28:29 +0530

    starred shows k stars ko peela kiya

--- src/components/styled.js
+++ src/components/styled.js
@@ -43,7 +43,11 @@ export const Star = styled.div`
   display: inline-block;
   width: 18px;
   height: 18px;
-  background-color: #ffc806;
+  /* background-color: #ffc806; */
+  background-color: ${(props) =>{
+    console.log(props)
+    return props.active?' #ffc806;' : '#ddd';
+  }};
   clip-path: polygon(
     50% 0%,
     61% 35%,
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -6,29 +6,35 @@ function showsReducer(prevState,action){
   switch(action.type)
   {
     case 'ADD' :{
+      console.log("state changed here");
       return  [...prevState,action.showId]
+
     }
 
     case 'REMOVE' : {
-      
+      console.log("state changed here");
       return prevState.filter( (showId)=> showId!==action.showId );
     } 
 
     default : return prevState
   }
+
 }
 
 function usePersistedReducer(reducer,initialState,key){
-  
+  console.log("rendered");
   const [state,dispatch] = useReducer(reducer,initialState,(initial)=>{
-
-      const persisted = localStorage.getItem(key);
+   
+    console.log("this is inital");
+   console.log(initial);
+     const persisted = localStorage.getItem(key);
 
       return persisted ? JSON.parse(persisted):initial
+   
   });  //what this function return is set as initialState 
 
    useEffect(()=>{
-      
+      console.log("useEffect called");
     localStorage.setItem(key,JSON.stringify(state))
    },[state,key])
 
@@ -37,5 +43,6 @@ function usePersistedReducer(reducer,initialState,key){
 
 
 export function useShows(key='shows'){
+  console.log("use shows called ");
   return usePersistedReducer(showsReducer,[],key);
 }
